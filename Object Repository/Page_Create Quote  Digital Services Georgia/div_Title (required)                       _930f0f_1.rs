8d40cffa6f7320bc88a04b8d567d3d67826938c3
amessenger <amessenger@localhost>
2019-12-26 12:06:04 -0500

    ROOT-2729--Test case for creating Quote node created

---
+++ Object Repository/Page_Create Quote  Digital Services Georgia/div_Title (required)                       _930f0f_1.rs
@@ -0,0 +1,145 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Title (required)                       _930f0f_1</name>
+   <tag></tag>
+   <elementGuidId>89089bbc-7980-434a-a2f4-a92a0625b516</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//form[@id='node-quote-form']/div/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>layout-region layout-region-node-main</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+    
+      
+      Title (required)
+        
+
+        
+
+  
+
+
+
+      
+  
+      Quote
+        
+  Rich Text Editor, Quote fieldEditor toolbarsFormatting BoldKeyboard shortcut Command+B ItalicKeyboard shortcut Command+ILinks LinkKeyboard shortcut Command+K UnlinkMedia Link to DocumentUtilities CopyKeyboard shortcut Command+C CutKeyboard shortcut Command+X PasteKeyboard shortcut Command+V Paste from Word Remove FormatOther Insert Special CharacterEnd Check AccessibilityAdmin-only SourcePress ALT 0 for helpElements pathbodyp 
+
+
+        
+About text formats
+
+
+
+
+  
+
+  
+
+        
+
+        Card
+          
+      Mark this if you want this component to be displayed with a card decoration around it.
+    
+  
+
+      
+  
+
+      Icon
+        Animal: DogCommunication: Envelope closedCommunication: PhoneFile: DocumentFile: ReadFile: WriteHealth ShieldHuman ear disabilityHuman eye disabilityMoney BillPerson BoyPerson Woman with BoyTravel: BusTravel: Car sideTravel: PlaneWater DropCalendarCommunication: QuestionGuidelinesHuman hand supportPerson Action Add ManTrophyBusiness: BuildingCommunication: ComputerEvent: Graduation CapEvent: Military GraveHome: BedObject: Apple on BookObject: GearPerson WomanHuman brainHuman wheelchairObject: Football helmetObject: Judge's gavelObject: Judge's robeObject: Military badgeWarning: Notification BellBusiness: BriefcaseWarning: TriangleHome: BuildingObject: LockTravel: Car backPerson ManCommunication: ConversationCommunication: EllipsisEnvironment: CloudEnvironment: TreePerson Action Add BoyPerson Action Add GirlPerson Action Add Old ManPerson Action Add Old WomanPerson Action Add WomanPerson GirlPerson Man with BoyPerson Man with GirlPerson Old ManPerson Old WomanPerson Woman with GirlAnimal: FishCommunication: RatingFood: VeggiesObject: FlagShape: Circle PlusShape: StarGeorgia: CapitolGeorgia: State outlineShape: CheckboxShape: Circle ArrowsTool: CalculatorTool: Paint RollerTravel: Drivers LicenseTravel: MapTravel: Map pinTravel: TruckEnvironment: RainEvent: Wedding Rings 1Event: Wedding Rings 2Money Arrow DownMoney Arrow UpNumber 1Number 2Number 3Number 4Object: GiftTravel: WheelWarning: Hazard CircleWarning: Hazardous Material- None -
+        
+
+      
+  
+      Attribution
+        
+
+        
+About text formats
+
+  Minimal
+  
+  
+      
+          
+                      Allowed HTML tags: &lt;a href hreflang name id> &lt;em> &lt;strong> &lt;p> &lt;br>
+                      Lines and paragraphs break automatically.
+                      Web page addresses and email addresses turn into links automatically.
+            
+    
+      
+  
+
+
+
+
+  
+
+  
+      
+  
+
+  </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;node-quote-form&quot;)/div[@class=&quot;layout-node-form clearfix&quot;]/div[@class=&quot;layout-region layout-region-node-main&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//form[@id='node-quote-form']/div/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Create Quote'])[2]/following::div[3]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='available updates'])[1]/following::div[5]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//form/div/div</value>
+   </webElementXpaths>
+</WebElementEntity>
