a33ee4dc1b072144b6143a43d40c80856b6d5e3e
Anthony Fournier <anthony.fournier@mediacurrent.com>
2020-01-06 16:53:21 -0500

    ROOT-2684: Added a test case to create a Listing Block to use in the Embed Listing Block test.

---
+++ Object Repository/Create Listing Block/Page_From Automation  Digital Services Georgia/main_Open  configuration optionsEdit.rs
@@ -0,0 +1,556 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>main_Open  configuration optionsEdit</name>
+   <tag></tag>
+   <elementGuidId>4d227755-4dd8-4a82-9f42-13cd7df8e2d8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='main-content']/div/div[3]/div/div[2]/main</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>main</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>content-page__main</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>role</name>
+      <type>Main</type>
+      <value>main</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                  
+  
+
+        
+            
+      Open  configuration optionsEdit
+    
+  
+  
+                 
+ 
+
+        
+  
+  
+  
+  
+  
+
+  
+  
+  
+
+                                                                                                                                                                
+  
+  
+                  
+          
+          
+        
+            
+  
+        Nikhil Deshpande
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+        Charronda King
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+        Pooja Berrong
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+        Paula Buchanan
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+        Amanda de Zayas
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+        Dominic Distretti
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+        Jasmyne Dove
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+        Jasmyne Epps
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+        Jasmyne Epps
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+        Jasmyne Epps
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+        Jasmyne Epps
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+        Jasmyne Epps
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+        Jasmyne Epps
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+        Mike Galifianakis
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+        Rachel Hart
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+        Jenine Ingram
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+        Kyle Johnson
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+        Martin Kester
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+        1 Display Name Overriding First Last (optional)
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+        2 Display Name Overriding First Last (optional)
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+                        
+          
+          
+        
+            
+  
+        3 Display Name Overriding First Last (optional)
+  
+  Open  configuration optionsEdit
+      
+  
+
+          
+  
+  
+
+        
+            
+
+
+
+  
+  
+
+  
+  
+
+  
+  
+
+
+
+  
+      
+  
+          
+      
+        
+              Link 1
+          
+  
+
+      
+
+  
+
+         
+        </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;main-content&quot;)/div[@class=&quot;content-page&quot;]/div[@class=&quot;content-page__mid&quot;]/div[@class=&quot;content-page__mid-main&quot;]/div[@class=&quot;content-page__main-primary&quot;]/main[@class=&quot;content-page__main&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='main-content']/div/div[3]/div/div[2]/main</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='From Automation'])[3]/following::main[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Configure block'])[4]/following::main[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//main</value>
+   </webElementXpaths>
+</WebElementEntity>
