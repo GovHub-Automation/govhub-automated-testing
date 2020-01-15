4dcb6179512f10a717ce31d40b60a83f57bc1828
Anthony Fournier <anthony.fournier@mediacurrent.com>
2020-01-15 15:41:19 -0500

    ROOT-2754: Added test suite to run all embed test cases.

---
+++ Test Suites/Embed Suite.ts
@@ -0,0 +1,103 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Embed Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0d191d08-f7de-4b71-9f9f-e2e1db7b5514</testSuiteGuid>
+   <testCaseLink>
+      <guid>1010bd73-ece3-46ed-9f26-1f1a1075750c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Embed Accordion</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8fb3aecc-3732-4e93-8cda-ef968ff92e6d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Embed Bio</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>4ab79763-402f-4c8f-8563-f5ef945ec9d7</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d0137f74-3d88-4459-89bb-703cfa7c9fa0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Embed CodeBlock</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9a953b36-58fe-4d48-b9f1-561f26f501f3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Embed Contact</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>291e51c3-d6d2-4e69-a455-e63d8aca35fa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Embed CTA</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>79fc5bba-293e-4b21-a9af-7a77177a9954</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Embed Doc</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>04f37f49-7151-46b8-abbc-a6c07405c95d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Embed Event</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b5f88129-5a53-4585-887e-518ce8b27756</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Embed Image</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>030626c5-e574-4bee-8637-b37ed73adafb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Embed LinkCollection</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>95c2e39b-9808-4acf-adf8-98ef372437a5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Embed ListingBlock</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4158472e-3038-4381-934d-4f5f8f81c682</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Embed News</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ba080bab-3b26-4bd8-970e-71cea88865c6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Embed Promo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>81114b78-c2e6-4c8e-915a-0c48ae194fea</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Embed Remote</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f6aa9e7c-bbf8-4aaa-be03-de445b87a03b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Embed Video</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
