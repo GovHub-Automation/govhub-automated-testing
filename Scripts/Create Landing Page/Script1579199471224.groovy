5f5db390aa6cd3ac344ebaf1712930cd1eaa2328
Anthony Fournier <anthony.fournier@mediacurrent.com>
2020-01-16 14:10:18 -0500

    ROOT-2702: Added the Create Landing Page test case.

---
+++ Scripts/Create Landing Page/Script1579199471224.groovy
@@ -0,0 +1,51 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.callTestCase(findTestCase('Authenticate Prod'), [:], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.callTestCase(findTestCase('Log In as Editor'), [:], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.click(findTestObject('Page_jasmyneepps  Digital Services Georgia/a_Content'))
+
+WebUI.click(findTestObject('Page_Content  Digital Services Georgia/a_Add content'))
+
+WebUI.click(findTestObject('Create Landing Page/Page_Add content  Digital Services Georgia/span_Landing Page'))
+
+WebUI.setText(findTestObject('Create Landing Page/Page_Create Landing Page  Digital Services Georgia/input_Title_title0value'), 
+    'From Automation')
+
+WebUI.setText(findTestObject('Create Landing Page/Page_Create Landing Page  Digital Services Georgia/input_Short Title_field_short_title0value'), 
+    'From Automation')
+
+WebUI.setText(findTestObject('Create Landing Page/Page_Create Landing Page  Digital Services Georgia/input_Label_field_landing_page_label0value'), 
+    'Label text')
+
+WebUI.click(findTestObject('Create Landing Page/Page_Create Landing Page  Digital Services Georgia/input_Include_navigation_checkbox'))
+
+WebUI.setText(findTestObject('Create Landing Page/Page_Create Landing Page  Digital Services Georgia/html_Rich Text Editor Summary field'), 
+    'Summary text')
+
+WebUI.selectOptionByValue(findTestObject('Create Landing Page/Page_Create Landing Page  Digital Services Georgia/select_DraftNeeds ReviewPublishedArchived'), 
+    'published', false)
+
+WebUI.click(findTestObject('Create Landing Page/Page_Create Landing Page  Digital Services Georgia/Save_button'))
+
+WebUI.verifyElementPresent(findTestObject('Create Landing Page/Page_From Automation  Digital Services Georgia/li_From Automation'), 
+    0)
+
+WebUI.closeBrowser()
+
