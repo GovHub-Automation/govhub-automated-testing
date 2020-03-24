3d423d964470d33e76ae30031bbe3b2c090a89e6
cwilson228 <47196792+cwilson228@users.noreply.github.com>
2020-03-25 07:25:56 +0900

    ROOT-2940 Remaining Script Files for Test Case

---
+++ Scripts/Manage Main Menu/Create Main Menu - Parent/Script1584967570331.groovy
@@ -0,0 +1,43 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+
+not_run: WebUI.callTestCase(findTestCase('Log Me In'), [:], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.click(findTestObject('Object Repository/Manage_Main Menu/a_Structure'))
+
+WebUI.click(findTestObject('Object Repository/Manage_Main Menu/a_Menus'))
+
+WebUI.click(findTestObject('Object Repository/Manage_Main Menu/Edit menu_MainNav'))
+
+WebUI.click(findTestObject('Object Repository/Manage_Main Menu/a_Edit menu'))
+
+WebUI.click(findTestObject('Object Repository/Manage_Main Menu/a_Add link'))
+
+WebUI.click(findTestObject('Object Repository/Manage_Main Menu/input_Title'))
+
+WebUI.setText(findTestObject('Object Repository/Manage_Main Menu/input_Title'), 'MCQA Test Menu Parent 1')
+
+WebUI.click(findTestObject('Object Repository/Manage_Main Menu/input_Link'))
+
+WebUI.setText(findTestObject('Object Repository/Manage_Main Menu/input_Link'), 'http://www.mediacurrent.com')
+
+WebUI.click(findTestObject('Object Repository/Manage_Main Menu/input_Description'))
+
+WebUI.setText(findTestObject('Object Repository/Manage_Main Menu/input_Description'), 'Test Menu Parent 1')
+
+WebUI.click(findTestObject('Object Repository/Manage_Main Menu/input_Save'))
+
