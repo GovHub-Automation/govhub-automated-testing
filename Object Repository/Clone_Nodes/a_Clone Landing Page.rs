f09b09c261dc040e12b8c50d883839673a23d2b6
cwilson228 <47196792+cwilson228@users.noreply.github.com>
2020-03-30 23:51:13 +0900

    ROOT-2941-Cloning Nodes

---
+++ Object Repository/Clone_Nodes/a_Clone Landing Page.rs
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Clone Landing Page</name>
+   <tag></tag>
+   <elementGuidId>e3a776c3-d9f6-44e2-bdcd-014ccbc7ceba</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[contains(@href, '/entity_clone/node/23946?destination=/admin/content%3Ftitle%3DMCQA%2520Test%2520Landing%2520Page%2520-%2520Cloning%26type%3DAll%26uid%3D%26field_division_target_id%3DAll%26moderation_state%3DAll')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/entity_clone/node/23946?destination=/admin/content%3Ftitle%3DMCQA%2520Test%2520Landing%2520Page%2520-%2520Cloning%26type%3DAll%26uid%3D%26field_division_target_id%3DAll%26moderation_state%3DAll</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>hreflang</name>
+      <type>Main</type>
+      <value>en</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Clone</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;views-form-content-page-1&quot;)/table[@class=&quot;vbo-table views-table views-view-table cols-8 responsive-enabled sticky-enabled&quot;]/tbody[1]/tr[2]/td[@class=&quot;views-field views-field-operations&quot;]/div[@class=&quot;dropbutton-wrapper dropbutton-multiple open&quot;]/div[@class=&quot;dropbutton-widget&quot;]/ul[@class=&quot;dropbutton&quot;]/li[@class=&quot;clone dropbutton-action secondary-action&quot;]/a[1]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//form[@id='views-form-content-page-1']/table/tbody/tr[2]/td[8]/div/div/ul/li[3]/a</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>(//a[contains(text(),'Clone')])[3]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='List additional actions'])[2]/following::a[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Edit'])[3]/following::a[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Move to Trash'])[3]/preceding::a[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Usage'])[2]/preceding::a[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '/entity_clone/node/23946?destination=/admin/content%3Ftitle%3DMCQA%2520Test%2520Landing%2520Page%2520-%2520Cloning%26type%3DAll%26uid%3D%26field_division_target_id%3DAll%26moderation_state%3DAll')]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//tr[2]/td[8]/div/div/ul/li[3]/a</value>
+   </webElementXpaths>
+</WebElementEntity>
